c13cac1654f8ee1415d0b46d31d14e9a76c2d3c1
MatiNeroda <76596445+MatiNeroda@users.noreply.github.com>
2020-12-25 20:26:42 +0100

    Aktualizacja pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
 let item = false
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+basic.pause(5000)
+maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 100)
+maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 100)
 basic.forever(function () {
     if (maqueen.sensor(PingUnit.Centimeters) < 35 && maqueen.sensor(PingUnit.Centimeters) != 0) {
         item = Math.randomBoolean()
@@ -8,7 +20,7 @@ basic.forever(function () {
             basic.pause(800)
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 100)
             maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 100)
-            basic.pause(300)
+            basic.pause(800)
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
         }
@@ -18,7 +30,7 @@ basic.forever(function () {
             basic.pause(800)
         }
     } else {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 120)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 120)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
     }
 })
