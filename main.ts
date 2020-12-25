cda28d992da7f67eb130e361d80dab397729f2a0
MatiNeroda <76596445+MatiNeroda@users.noreply.github.com>
2020-12-25 20:22:16 +0100

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,14 @@ basic.forever(function () {
     if (maqueen.sensor(PingUnit.Centimeters) < 35 && maqueen.sensor(PingUnit.Centimeters) != 0) {
         item = Math.randomBoolean()
         if (item == true) {
-            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
             maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
             basic.pause(800)
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 100)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 100)
+            basic.pause(300)
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
         }
         if (item == false) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
@@ -13,7 +18,7 @@ basic.forever(function () {
             basic.pause(800)
         }
     } else {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 120)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 120)
     }
 })
